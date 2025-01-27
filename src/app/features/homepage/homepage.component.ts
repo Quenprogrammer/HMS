914050ebca8cfd2b117e0597e343ae7fe65a4adb
QuenProgrammer <adamsadam3667@gmail.com>
2025-01-27 12:16:45 -0800

    added new homepage Contents

--- src/app/features/homepage/homepage.component.ts
+++ src/app/features/homepage/homepage.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {NewsCardComponent} from "../news/news-card/news-card.component";
 import {News} from "../news/news";
-import {heading1, heading1Tagline, heading2} from "../../siteContent/homePage";
+import {heading1, heading1Tagline, heading2, whoWeAre} from "../../siteContent/homePage";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
   imports: [
-    NewsCardComponent
+    NewsCardComponent,
+    NgForOf
   ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
@@ -18,4 +20,5 @@ export class HomepageComponent {
   protected readonly heading1 = heading1;
   protected readonly heading1Tagline = heading1Tagline;
   protected readonly heading2 = heading2;
+  protected readonly whoWeAre = whoWeAre;
 }
